e38b5cf8d0b7eedd72243ed48dbbd706166c10fb
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-20 23:18:00 -0400

    feat: add operators type

---
+++ operators.py
@@ -0,0 +1,8 @@
+value_first = 100
+value_second = 200
+other_value = 100
+
+print(value_first < value_second and value_first == 1000)
+print(value_first < value_second or value_first == 1000)
+print(value_first > value_second or value_first != 100)
+print(value_first is other_value)
