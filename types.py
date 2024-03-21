10bc80c37f66f45b8af2c985eacc96d1e783d558
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-20 23:16:26 -0400

    feat: add data types file

---
+++ types.py
@@ -0,0 +1,16 @@
+import math
+
+print(1)
+print(dir())
+# print(help())
+
+X = 'man'
+print (10 / 2)
+print (10 // 2)
+print (int(10 / 2))
+print(int(10.9))
+print(float(10))
+print (f'Talk to me {X}')
+nome = input('Whats yout name: ')
+
+print(f'...\nMy name is {nome}')
