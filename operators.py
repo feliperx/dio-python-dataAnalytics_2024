c49f845850634187fb2306d61dd2dd127bad4919
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-26 22:33:27 -0400

    feat: add new print

--- operators.py
+++ operators.py
@@ -1,8 +1,12 @@
 value_first = 100
 value_second = 200
 other_value = 100
+text = 'Felipe é formado'
+
 
 print(value_first < value_second and value_first == 1000)
 print(value_first < value_second or value_first == 1000)
 print(value_first > value_second or value_first != 100)
 print(value_first is other_value)
+print('felipe' in text)
+print('Felipe' in text)
