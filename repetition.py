341efc4b454806e591fd128193c9eeb6241dc478
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-26 23:19:59 -0400

    feat: add while to repetition file

--- repetition.py
+++ repetition.py
@@ -4,3 +4,20 @@ VOWELS = "AEIOU"
 for character in text:
     if character.upper() in VOWELS:
         print(character, end=" ")
+print()
+
+#range
+for number in range(0, 11):
+    print(number, end=" ")
+
+print()
+
+for odd_number in range(1, 25, 2):
+    print(odd_number, end=" ")
+
+print("\n")
+
+#while
+opcao = 1
+while opcao != 0:
+    opcao = int(input('>>>> Informe a opcao <<<<\n[1] Continuar\n[0] Sair\n'))
