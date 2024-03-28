3a7a29d155b71d3540523e5c8105b8f8dfa8ee4d
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-27 23:34:04 -0400

    feat: add strings-manipulation file

---
+++ strings-manipulation.py
@@ -0,0 +1,14 @@
+text = " # Back PRopagation"
+
+print(text.strip())
+print(text.lstrip())
+print(text.rstrip())
+
+print("-".join(text))
+
+print(text.upper())
+print(text.lower())
+print(text.lower())
+print(2*text.lower())
+
+print(f'Ola {text}')
