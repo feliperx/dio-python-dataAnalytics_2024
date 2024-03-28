a274ef0aacae539d027967320b59c34c311c8e50
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-28 02:16:57 -0400

    feat: add string manipulation

--- strings-manipulation.py
+++ strings-manipulation.py
@@ -12,3 +12,14 @@ print(text.lower())
 print(2*text.lower())
 
 print(f'Ola {text}')
+
+print(text[1])
+print(text[2:10])
+print(text[2:11:2]) #do 2 ate 10 pulando de 2
+print(text[::-1])
+
+print(f'''
+
+      oi oi oi {text}
+
+      ''')
