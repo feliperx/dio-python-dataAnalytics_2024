e2f330dda3f1beb5330ea5a12e0dcb7a4d342d96
Felipe Xaud <feliipe.xaud@gmail.com>
2024-03-29 23:53:20 -0400

    feat: add tuples file

---
+++ tuples.py
@@ -0,0 +1,15 @@
+fruit = ('melon',)
+
+fruits = ('apple', 'banana', 'watermelon',)
+
+footbal_club = tuple('Vasco')
+
+footbal_clubs = tuple(['Vasco', 'Real Madrid', 'Boca Juniors'])
+
+numbers = tuple([[1,2,3], 'vava'],)
+
+print(fruit)
+print(fruits)
+print(footbal_club)
+print(footbal_clubs)
+print(numbers)
