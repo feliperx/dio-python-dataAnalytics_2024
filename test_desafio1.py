a5ad63ccfe252f0e007db826b21da1cb97515178
Felipe Xaud <feliipe.xaud@gmail.com>
2024-04-12 18:37:52 -0400

    feat: add test_desafio1 file

---
+++ test_desafio1.py
@@ -0,0 +1,8 @@
+capacidade_atual, aumento_percentual = map(int, input().split())
+
+#TODO: Calcule a nova capacidade do disco de Mithril
+capacidade_final = int(capacidade_atual * (aumento_percentual / 100))
+
+#TODO: Imprima a nova capacidade
+
+print(capacidade_final)
