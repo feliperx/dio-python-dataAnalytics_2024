c77d9b586063fe7a408aa3169db340c0e61d83b8
Felipe Xaud <feliipe.xaud@gmail.com>
2024-10-13 17:57:30 -0400

    feat: add datetime example

---
+++ date_time/1_datetime.py
@@ -0,0 +1,10 @@
+from datetime import date, datetime, time
+
+data_aniversario = date(2024, 10, 2)
+data_hoje = date.today()
+print(data_aniversario)
+print(data_hoje)
+
+data_hora_hoje = datetime.today()
+print(data_hora_hoje)
+
